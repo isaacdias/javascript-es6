376a5590a945da815535bfbb41d7ccdd5bf0dc23
Isaac Dias <isaacardias@gmail.com>
2020-05-25 19:42:02 -0300

    correção do enunciado do exercicio

--- conceitos/exercicio01.js
+++ conceitos/exercicio01.js
@@ -1,11 +1,11 @@
 /*
-1 - Para testar seus conhecimentos com classes, crie uma classe com nome "Admin", 
+Para testar seus conhecimentos com classes, crie uma classe com nome "Admin", 
 essa classe deveextender uma segunda classe chamada "Usuario".A classe usuário deve 
 receber dois parâmetros no método construtor, e-mail e senha, e anotá-losem propriedades da classe. 
 A classe "Admin" por sua vez não recebe parâmetros mas deve repassar os parâmetros de e-mail e senha 
 à classe pai e marcar uma propriedade "admin" comotrue na classe.Agora com suas classes formatadas, 
 adicione um método na classe Usuario chamado isAdmin queretorna se o usuário é administrador ou não
- baseado na propriedade admin ser true ou não.
+baseado na propriedade admin ser true ou não.
 */
 
 // const User1 = new Usuario('email@teste.com', 'senha123');
@@ -34,5 +34,5 @@ class Admin extends Usuario {
 const User1 = new Usuario('email@teste.com', '123456');
 const Adm1 = new Admin('email@teste.com', '123456');
 
-console.log(User1.isAdmin()) // false
+console.log(User1.isAdmin()) 
 console.log(Adm1.isAdmin())
