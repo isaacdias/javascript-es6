b360a174e6192b51f29e16c6a05c115f205888e7
Isaac Dias <isaacardias@gmail.com>
2020-05-26 19:30:28 -0300

    configurando o Webpack

--- javascript_es6/main.js
+++ javascript_es6/main.js
@@ -1,31 +1,3 @@
-class List {
-    constructor() {
-        this.data = [];
-    }
+import { soma } from './funcoes';
 
-    add(data) {
-        this.data.push(data);
-        console.log(this.data);
-    }
-}
-
-class TodoList extends List {
-    constructor() {
-        super();
-
-        this.user = 'Isaac';     
-    }
-
-    showUser() {
-        console.log(this.user);  
-    }
-}
-
-const MyList = new TodoList();
-
-document.getElementById('novo-todo').onclick = function () {
-    MyList.add('Novo todo');
-}
-
-MyList.showUser();
-
+console.log(soma(1, 5));
